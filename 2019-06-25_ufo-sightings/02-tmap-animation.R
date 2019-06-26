1b2adaf7f0d468d76b455a7afc99902705a1a7c5
Jesus M. Castagnetto <jesus@castagnetto.com>
2019-06-26 06:01:28 -0500

    final version (famous last words)

--- 2019-06-25_ufo-sightings/02-tmap-animation.R
+++ 2019-06-25_ufo-sightings/02-tmap-animation.R
@@ -31,22 +31,34 @@ yrs <- unique(ufo$yr)
 countries <- World %>%
   filter(iso_a3 %in% ufo$iso_a3)
 
-mk_tmap_anim <- function(df, years) {
+breaks <- seq(0, 7000, by = 1000)
+
+mk_tmap_anim <- function(basemap, df, breaks, years) {
   df <- subset(df, yr %in% years)
   fname <- paste0("animation_", min(years), "-", max(years), ".gif")
-  map1 <- tm_shape(countries) +
+  map1 <- tm_shape(basemap) +
     tm_polygons(NA) +
     tm_shape(df) +
-    tm_polygons("n") +
+    tm_polygons("n", breaks = breaks, title = "Number of sightings") +
     tm_facets(along = "yr", free.coords = FALSE, free.scales = FALSE)
   tmap_animation(map1,
                  filename = here::here("2019-06-25_ufo-sightings/", fname))
 }
 
-mk_tmap_anim(ufo, yrs[1:20])
-mk_tmap_anim(ufo, yrs[21:40])
-mk_tmap_anim(ufo, yrs[41:60])
-mk_tmap_anim(ufo, yrs[61:83])
+mk_tmap_anim(countries, ufo, breaks, yrs[1:20])
+mk_tmap_anim(countries, ufo, breaks, yrs[21:40])
+mk_tmap_anim(countries, ufo, breaks, yrs[41:60])
+mk_tmap_anim(countries, ufo, breaks, yrs[61:83])
 
+# small multiples (static) from 2011-2014 (for twitter)
+multmap <- tm_shape(countries) +
+  tm_polygons(NA) +
+  tm_shape(ufo %>% filter(yr %in% yrs[80:83])) +  # 2011-2014
+  tm_polygons("n", breaks = breaks, title = "Number of sightings") +
+  tm_facets(by = "yr", free.coords = FALSE, free.scales = FALSE, ncol = 2) +
+  tm_layout(legend.position = c("right", "bottom"))
 
+tmap_save(multmap,
+          filename = here::here("2019-06-25_ufo-sightings/tmap-facets-ufo-sightings.png"),
+          width = 1200, height = 600)
 
---
+++ 2019-06-25_ufo-sightings/README.md
@@ -0,0 +1,8 @@
+- Had to make several GIF files, otherwise my computer couldn't finish processing
+- The big GIF was made using `gifsicle`
+
+```
+$ gifsicle animation_* --colors 256 > animation-ufo-sightings_1910-2014.gif
+```
+
+- The small multiples plot is so I had something to put on a tweet (the animations didn't load on Twitter)
