4f465b439e797dc986b224d5d520b3a96819f492
Jesus M. Castagnetto <jesus@castagnetto.com>
2019-07-16 09:30:44 -0500

    calendar chart finished for now

--- 2019-07-16-r4ds-members/02-calendar-chart.R
+++ 2019-07-16-r4ds-members/02-calendar-chart.R
@@ -5,16 +5,43 @@ load(
   here::here("2019-07-16-r4ds-members/data/r4ds_members.Rdata")
 )
 
-r4ds_members %>%
+calchart <- r4ds_members %>%
   mutate(
     pct_active = daily_active_members / total_membership,
+    pct_msg = daily_members_posting_messages / daily_active_members,
     year = lubridate::year(date)
   ) %>%
   group_by(year) %>%
   e_charts(date) %>%
-  e_calendar(range = "2017", top = "40") %>%
+  e_title(
+    text = "Percentage of active members who post messages",
+    subtext = "#TidyTuesday: 2019-07-16 R4DS Members",
+    sublink = "https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-07-16"
+  ) %>%
+  e_text_g(
+    style = list(
+      text = "@jmcastagnetto (Jesus M. Castagnetto)"
+    ),
+    left = "70%",
+    top = 600
+    ) %>%
+  e_calendar(range = "2017", top = "80") %>%
   e_calendar(range = "2018", top = "260") %>%
-  e_calendar(range = "2019", top = "480") %>%
-  e_heatmap(pct_active, coord_system = "calendar") %>%
-  e_visual_map(pct_active) %>%
-  e_tooltip("item", formatter = e_tooltip_item_formatter("percent"))
+  e_calendar(range = "2019", top = "440") %>%
+  e_heatmap(pct_msg,
+            coord_system = "calendar") %>%
+  e_visual_map(pct_msg,
+               formatter = e_axis_formatter("percent"),
+               orient = "horizontal",
+               left = "center",
+               top = 600) %>%
+  e_tooltip("item",
+            formatter = e_tooltip_item_formatter("percent")) %>%
+  e_toolbox_feature()
+
+htmlwidgets::saveWidget(
+  widget = calchart,
+  file =  here::here("2019-07-16-r4ds-members/calendar-chart.html"),
+  title = "Calendar chart"
+)
+
