381c5afa7be43acf8cbabe3d65b467f01c78198d
Jesus M. Castagnetto <jesus@castagnetto.com>
2019-07-15 22:10:54 -0500

    Revert "unifished experiments"
    This reverts commit 762dbdb662dff11040ada585116f23cf175f27b3.

--- 2019-07-16-r4ds-members/01-get-data.R
+++
@@ -1,7 +0,0 @@
-r4ds_members <- readr::read_csv(
-  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-16/r4ds_members.csv"
-)
-save(
-  r4ds_members,
-  file = here::here("2019-07-16-r4ds-members/data/r4ds_members.Rdata")
-)
