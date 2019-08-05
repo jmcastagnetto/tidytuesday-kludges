041321862fef35be325937b8fc4d6db3c49524ef
Jesus M. Castagnetto <jesus@castagnetto.com>
2019-08-05 08:39:27 -0500

    txtplots finished

--- 2019-07-30_video-games/04-text-plots.R
+++ 2019-07-30_video-games/04-text-plots.R
@@ -5,27 +5,45 @@ load(
   here::here("2019-07-30_video-games/data/video-games.Rdata")
 )
 
-tmp <- video_games %>% filter(!is.na(release_year))
-txtdensity(tmp$release_year)
-txtbarchart(as.factor(tmp$release_year), pch = "#",
-            ylab = "Games")
+# Number of games per release year
+games_year <- video_games %>%
+  filter(!is.na(release_year)) %>%
+  group_by(release_year) %>%
+  tally()
 
-txtplot(tmp$price,
-        tmp$metascore, xlab = "Price", ylab = "Metascore")
-txtcurve(1*x, from = 0, to = 70)
+txtplot(x = games_year$release_year,
+        y = games_year$n,
+        xlab = "Release Year",
+        ylab = "Games",
+        xlim = c(2000, 2020))
+
+# Metascores vs price
+txtplot(x = video_games$price,
+        y = video_games$metascore,
+        xlab = "Price",
+        ylab = "Metascore")
 
 
-pr1 <- tmp %>% filter(price_range == "(0,20]")
-pr2 <- tmp %>% filter(price_range == "(20,40]")
-pr3 <- tmp %>% filter(price_range == "(40,600]")
-
-range_0_20 <- pr1$metascore
-range_20_40 <- pr2$metascore
-range_40_600 <- pr3$metascore
+# Metascore vs price range
+range_0_20 <- (video_games %>%
+                 filter(price_range == "(0,20]"))$metascore
+range_20_40 <- (video_games %>%
+                  filter(price_range == "(20,40]"))$metascore
+range_40_600 <- (video_games
+                 %>% filter(price_range == "(40,600]"))$metascore
 
 txtboxplot(
   range_0_20, range_20_40, range_40_600,
-  xlab = "Metascore by price ranges"
+  xlab = "Metascore distribution by price range"
 )
 
-txtdensity(tmp$metascore)
+# 95% quantile for price
+quantile(video_games$price, probs = 0.97, na.rm = TRUE)
+# Price distribution up to 99% quantile
+# pricing "sweet spots" ~ 1, 5, 10, 15, 20, 30 (USD)
+txtdensity(
+  (video_games
+   %>% filter(!is.na(price) & price < 30))$price,
+  xlab = "Price (USD)"
+)
+
