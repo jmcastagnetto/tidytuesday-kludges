a537ce6e5aaf634b1cdd4171fe01fd271c2cc4c8
Jesus M. Castagnetto <jesus@castagnetto.com>
2019-10-22 16:04:24 -0500

    playing a bit more with networks

--- 2019-10-22_horror-movies/02-build-edges-nodes.R
+++ 2019-10-22_horror-movies/02-build-edges-nodes.R
@@ -1,39 +1,46 @@
-  nodes <- horror_movies_actor %>%
-    select(actor) %>%
-    distinct() %>%
-    mutate(
-      id = labels(actor)
-    ) %>%
-    filter(!is.na(actor))
+library(tidyverse)
+library(igraph)
 
-  edges <- horror_movies_actor %>%
-    mutate(
-      from_id = labels(actor)
-    ) %>%
-    group_by(title) %>%
-    mutate(
-      to_actor = lead(actor)
-    ) %>%
-    ungroup() %>%
-    rename(
-      from_actor = actor
-    ) %>%
-    select(
-      from_actor, from_id, to_actor
-    ) %>%
-    left_join(
-      nodes,
-      by = c("to_actor" = "actor")
-    ) %>%
-    rename(
-      to_id = id
-    ) %>%
-    filter(
-      !is.na(to_actor)
-    )
+load(
+  here::here("2019-10-22_horror-movies/horro-movies.Rdata")
+)
 
-  save(
-    nodes, edges,
+nodes <- horror_movies_actor %>%
+  select(actor) %>%
+  distinct() %>%
+  mutate(id = labels(actor)) %>%
+  filter(!is.na(actor)) %>%
+  select(actor, id)
+
+edges <- horror_movies_actor %>%
+  group_by(title) %>%
+  mutate(to_actor = lead(actor)) %>%
+  ungroup() %>%
+  rename(from_actor = actor) %>%
+  select(from_actor, to_actor) %>%
+  left_join(nodes,
+            by = c("from_actor" = "actor")) %>%
+  rename(from_id = id) %>%
+  left_join(nodes,
+            by = c("to_actor" = "actor")) %>%
+  rename(to_id = id) %>%
+  filter(!is.na(from_actor) & !is.na(to_actor)) %>%
+  group_by(from_actor, to_actor, from_id, to_id) %>%
+  tally() %>%
+  rename(
+    weight = n
+  ) %>%
+  select(from_actor, to_actor, from_id, to_id, weight)
+  #select(from_id, to_id, from_actor, to_actor, weight)
+
+net <- graph_from_data_frame(
+  d = edges,
+  vertices = nodes,
+  directed = FALSE
+)
+
+save(
+    nodes, edges, net,
     file = here::here("2019-10-22_horror-movies/actors-network.Rdata")
-  )
+)
 
---
+++ 2019-10-22_horror-movies/03-explore-actors-network.R
@@ -0,0 +1,90 @@
+library(tidyverse)
+library(igraph)
+
+load(
+  here::here("2019-10-22_horror-movies/actors-network.Rdata")
+)
+
+# centrality of the network
+
+centr_degree(net)$centralization
+centr_clo(net)$centralization
+centr_betw(net)$centralization
+centr_eigen(net)$centralization
+
+# > centr_degree(net)$centralization
+# [1] 0.001439259
+# > centr_clo(net)$centralization
+# [1] 4.801063e-05
+# > centr_betw(net)$centralization
+# [1] 0.08419203
+# > centr_eigen(net)$centralization
+# [1] 0.9978583
+
+diameter(net)
+
+# > diameter(net)
+# [1] 96
+
+
+cl <- components(net)
+cliq <- largest_cliques(net)
+
+# calculate connections for each node
+V(net)$indeg <- degree(net)
+V(net)[V(net)$indeg == max(V(net)$indeg)]
+
+connected_actors <- tibble(
+  id = V(net)$id,
+  actor = V(net)$name,
+  connections = V(net)$indeg
+) %>%
+  arrange(desc(connections), actor)
+
+# vertex betweenness
+V(net)$btwn <- betweenness(net)
+
+btwn_actors <- tibble(
+  id = V(net)$id,
+  actor = V(net)$name,
+  btwn = V(net)$btwn
+) %>%
+  arrange(desc(btwn), actor)
+
+E(net)$btwn <- edge_betweenness(net)
+
+edge_btwns <- tibble(
+  edge = attr(E(net), "vnames"),
+  btwn = E(net)$btwn
+) %>%
+  rownames_to_column() %>%
+  arrange(
+    desc(btwn), edge
+  )
+
+# closeness
+V(net)$clsns <- closeness(net)
+
+clsns_actors <- tibble(
+  id = V(net)$id,
+  actor = V(net)$name,
+  clsns = V(net)$clsns
+) %>%
+  arrange(desc(clsns), actor)
+
+# page rank
+
+#V(net)$pgrank <- page.rank(net)
+#
+#pgrank_actors <- tibble(
+#  id = V(net)$id,
+#  actor = V(net)$name,
+#  pgrank = V(net)$pgrank
+#)
+
+save(
+  cliq, cl, net, connected_actors, btwn_actors,
+  clsns_actors, edge_btwns, 
+  #pgrank_actors,
+  file = here::here("2019-10-22_horror-movies/actors-network-metrics.Rdata")
+)
