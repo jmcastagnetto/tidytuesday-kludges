b674f7f61152d4e12c7cb6dc7d15b09de7d03228
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-05-14 22:29:47 -0500

    raw data for the volcano eruptions

---
+++ 2020-05-12_volcano-eruptions/README.md
@@ -0,0 +1,1 @@
+https://github.com/rfordatascience/tidytuesday/blob/master/data/2020/2020-05-12/readme.md
---
+++ 2020-05-12_volcano-eruptions/get_data.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+
+volcano <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/volcano.csv')
+eruptions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/eruptions.csv')
+events <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/events.csv')
+tree_rings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/tree_rings.csv')
+sulfur <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/sulfur.csv')
+
+save(
+	 volcano,
+	 eruptions,
+	 events,
+	 tree_rings,
+	 sulfur,
+	 file = here::here("2020-05-12_volcano-eruptions/raw-data.Rdata")
+	 )
