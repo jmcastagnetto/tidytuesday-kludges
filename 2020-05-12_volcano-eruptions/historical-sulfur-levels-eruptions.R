1fd2f3ade2e8ccc1c550c87af3bad597af6f979b
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-05-16 17:31:57 -0500

    final version of historical sulfur and eruptions

--- 2020-05-12_volcano-eruptions/historical-sulfur-levels-eruptions.R
+++ 2020-05-12_volcano-eruptions/historical-sulfur-levels-eruptions.R
@@ -1,5 +1,14 @@
 library(tidyverse)
+library(lubridate)
+library(glue)
+
 load(here::here("2020-05-12_volcano-eruptions/raw-data.Rdata"))
+
+volcano_north_south <- volcano %>%
+  select(volcano_number, latitude, longitude) %>%
+  mutate(
+    Hemisphere = if_else(latitude > 0, "Northern", "Southern")
+  )
 
 sulfur_long <- sulfur %>%
   mutate(
@@ -13,34 +22,75 @@ sulfur_long <- sulfur %>%
   ) %>%
   filter(!is.na(year)) %>%
   mutate(
-    source = str_to_upper(source)
+    source = ifelse(source == "neem",
+                    "NEEM: Ice cores from Greenland",
+                    "WDDC: Ice cores from Antartica")
   )
 
 eruptions_sulfur <- eruptions %>%
   filter(between(start_year,
                  min(sulfur_long$year, na.rm = TRUE),
-                 max(sulfur_long$year, na.rm = TRUE)))
+                 max(sulfur_long$year, na.rm = TRUE))) %>%
+  left_join(
+    volcano_north_south,
+    by = "volcano_number"
+  ) %>%
+  filter(!is.na(Hemisphere)) %>%
+  mutate(
+    yr_dec = decimal_date(
+      ymd(
+        glue("{sprintf('%04d', y)}-{sprintf('%02d',m)}-{sprintf('%02d',d)}",
+             y = start_year,
+             m = if_else(is.na(start_month) | start_month == 0,
+                         1, start_month),
+             d = if_else(is.na(start_day) | start_day == 0,
+                         1, start_day)
+            )
+      )
+    )
+
+  )
 
 
 ggplot(data = eruptions_sulfur) +
   geom_vline(aes(xintercept = start_year),
-             color = "darkgrey",
-             linetype = "dashed") +
-  geom_count(aes(x = start_year,
-                 y = 200)) +
+             color = "orange", alpha = 0.5,
+             linetype = "dashed", show.legend = TRUE) +
+  geom_count(data = eruptions_sulfur %>%
+               filter(Hemisphere == "Northern"),
+             aes(x = start_year,
+                 y = 180, color = Hemisphere)) +
+  geom_count(data = eruptions_sulfur %>%
+               filter(Hemisphere == "Southern"),
+             aes(x = start_year,
+                  y = 150, color = Hemisphere)) +
+  scale_color_brewer(type = "qual", palette = "Set1") +
   geom_line(data = sulfur_long,
             aes(x = year, y = sulfur,
-                group = source, color = source),
+                group = source),
+            color = "blue",
             show.legend = FALSE) +
   scale_size(name = "Number of\neruptions") +
-  ggdark::dark_theme_minimal(20) +
-  #scale_y_log10() +
-  #annotation_logticks(sides = "l") +
+  theme_minimal(14) +
+  theme(
+    strip.text = element_text(face = "bold"),
+    panel.grid = element_line(color = "grey90"),
+    plot.title.position = "plot",
+    plot.caption.position = "plot"
+  ) +
   facet_wrap(~source, ncol = 1) +
+  ylim(0, 220) +
   labs(
     x = "Year",
-    y = "Sulfur concentration in ng/gr",
+    y = "Sulfur concentration [ng/gr]",
     title = "Historical sulfur content and eruptions (500 CE - 706 CE)",
-    subtitle = "#TidyTuesday, 2020-05-12 ('Volcano Eruptions'), from two sources:   ",
-    caption = "Code: // @jmcastagnetto, Jesus M. Castagnetto"
+    subtitle = "#TidyTuesday, 2020-05-12 (\"Volcano Eruptions\")",
+    caption = "Code: https://bit.ly/2LBpK3U // @jmcastagnetto, Jesus M. Castagnetto"
   )
+
+ggsave(
+  filename = here::here("2020-05-12_volcano-eruptions",
+                        "historical-sulfur-levels-eruptions.png"),
+  width = 10,
+  height = 6
+)
